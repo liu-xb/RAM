205b710213cc72d100a3cc074ff992e3c2925a5c
liu-xb <liu-xb@qq.com>
2018-06-25 16:45:09 +0800

    Add files via upload

--- train_veri.sh
+++ train_veri.sh
@@ -1,4 +1,17 @@
-caffe/build/tools/caffe train -solver prototxt/solver.prototxt -weights ~/caffemodel/vgg-cnn-m/VGG_CNN_M.caffemodel -gpu 0
+caffe/build/tools/caffe train -solver prototxt/solver_CN.prototxt -weights ~/caffemodel/vgg-cnn-m/VGG_CNN_M.caffemodel -gpu 0
 
-caffe/build/tools/caffe train -solver prototxt/solver_finetune.prototxt -weights snapshot/veri-attribute_iter_80000.caffemodel -gpu 0
+caffe/build/tools/caffe train -solver prototxt/solver_CN_finetune.prototxt -weights snapshot/veri-CN_iter_60000.caffemodel -gpu 0
 
+caffe/build/tools/caffe train -solver prototxt/solver_BN.prototxt -weights snapshot/veri-CN-finetune_iter_60000.caffemodel -gpu 0
+
+caffe/build/tools/caffe train -solver prototxt/solver_BN_finetune.prototxt -weights snapshot/veri-BN_iter_60000.caffemodel -gpu 0
+
+caffe/build/tools/caffe train -solver prototxt/solver_BN+R.prototxt -weights snapshot/veri-BN-finetune_iter_60000.caffemodel -gpu 0
+
+caffe/build/tools/caffe train -solver prototxt/solver_BN+R_finetune.prototxt -weights snapshot/veri-BN+R_iter_60000.caffemodel -gpu 0
+
+caffe/build/tools/caffe train -solver prototxt/solver_RAM.prototxt -weights snapshot/veri-BN+R-finetune_iter_60000.caffemodel -gpu 0
+
+caffe/build/tools/caffe train -solver prototxt/solver_RAM_finetune.prototxt -weights snapshot/veri-RAM_iter_60000.caffemodel -gpu 0
+
+echo "The final model is \"veri-RAM-finetune_iter_60000.caffemodel\". "
